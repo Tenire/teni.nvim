9eef47e199a1e7d986dc7dfb1258acc575f3b09b
Tenire <i@tenire.com>
2026-01-28 00:04:14 +0800

    fix: migrate leap.nvim source to Codeberg
    The repository has moved from GitHub to Codeberg. Updated the URL in init.lua to ensure the plugin can be installed and updated.

--- nvim/init.lua
+++ nvim/init.lua
@@ -79,7 +79,7 @@ require('lazy').setup({
     priority = 999,
   },
   {
-    'ggandor/leap.nvim',
+    url = 'https://codeberg.org/andyg/leap.nvim',
     config = function()
       vim.keymap.set({ 'n', 'x', 'o' }, 's', '<Plug>(leap)')
       vim.keymap.set('n', 'S', '<Plug>(leap-from-window)')
